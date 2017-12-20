7644392d73aa7de821333f173b2a4612960ee361
Erick <erickmwazonga@gmail.com>
2017-12-21 00:11:32 +0300

    Modified Email_backend

--- Coke/settings/base.py
+++ Coke/settings/base.py
@@ -169,7 +169,7 @@ ADMINS = [
 ]
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
 EMAIL_HOST = config('EMAIL_HOST', default='localhost')
 EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
